5481a84ef3cadd920c5f412db316ecc820cd69bb
byterID <artemijgladkov44644@gmail.com>
2023-01-10 17:13:08 +0500

    Updated Project

--- Assets/Resources/scripts/Menu.cs
+++ Assets/Resources/scripts/Menu.cs
@@ -3,9 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject buttonsMenu;
-    
+    public GameObject SoundSettings;
+    public GameObject GraphicsSettings;
+    public GameObject Achievments;
+
+
     public void ExitGame()
     {
         Application.Quit();
@@ -19,5 +22,23 @@ public class Menu : MonoBehaviour
     public void LoadMain()
     {
         SceneManager.LoadScene("startroom");
+    }
+    public void SetSoundActive()
+    {
+        SoundSettings.SetActive(true);
+        GraphicsSettings.SetActive(false);
+        Achievments.SetActive(false);
+    }
+    public void SetGraphicsActive()
+    {
+        SoundSettings.SetActive(false);
+        GraphicsSettings.SetActive(true);
+        Achievments.SetActive(false);
+    }
+    public void SetAchievmentsActive()
+    {
+        SoundSettings.SetActive(false);
+        GraphicsSettings.SetActive(false);
+        Achievments.SetActive(true);
     }
 }
